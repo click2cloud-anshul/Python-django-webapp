544df631489d4d6880127d712df03906fa90b119
click2cloud-anshul <68548740+click2cloud-anshul@users.noreply.github.com>
2021-03-05 11:30:09 +0530

    db connection changes

--- django_webapp/settings.py
+++ django_webapp/settings.py
@@ -77,18 +77,30 @@ WSGI_APPLICATION = 'django_webapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'ambabu',
+#         'PASSWORD': 'testdb',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+
+#     }
+# }
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'ambabu',
-        'PASSWORD': 'testdb',
+        'NAME': 'pgdb',
+        'USER': 'postgres',
+        'PASSWORD': 'anshdb',
         'HOST': 'localhost',
-        'PORT': '5432'
+        'PORT': '5454'
 
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
